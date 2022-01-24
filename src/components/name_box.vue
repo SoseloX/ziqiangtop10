d38fc874c05d0c5a5495adf4310f0bb99ebe38c6
illusoryw <98226453+illusoryw@users.noreply.github.com>
2022-01-24 11:32:28 +0800

    往年回顾页面1

---
+++ src/components/name_box.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="name-box-bg">{{name}}</div>
+</template>
+
+<script>
+export default {
+    name: "name_box",
+    props: ["name"]
+}
+</script>
+
+<style>
+.name-box-bg {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: rgba(230, 230, 230, .7);
+    border: 2px solid white;
+    box-shadow: inset 0px 0px 5px 1px #AAA, 1px 1px 5px 1px #AAA;
+    font-size: 28px;
+    padding: 15px;
+    margin: 15px;
+}
+</style>
--- src/views/Review.vue
+++ src/views/Review.vue
@@ -1,15 +1,71 @@
 <template>
     <div id='contanier'>
-
+        <router-link id='back_button' to="./index">
+            <img src="../assets/back.png" id="back">
+        </router-link>
+        <div class="review_head">
+            <img src="../assets/big_cloud.png" id="review_big_cloud_left" class="big_cloud">
+            <img src="../assets/review_text.png" id="review_text">
+            <img src="../assets/big_cloud.png" id="review_big_cloud_right" class="big_cloud">
+        </div>
+        <div id="name-year">2018</div>
+        <div class="name-table">
+            <name_box name="姓名1"></name_box>
+            <name_box name="姓名2"></name_box>
+            <name_box name="姓名3"></name_box>
+            <name_box name="姓名4"></name_box>
+            <name_box name="姓名5"></name_box>
+            <name_box name="姓名6"></name_box>
+            <name_box name="姓名7"></name_box>
+            <name_box name="姓名8"></name_box>
+            <name_box name="姓名9"></name_box>
+            <name_box name="姓名A"></name_box>
+        </div>
     </div>
 </template>
 
 <script>
-export default{
-    name:'Review'
+import name_box from '../components/name_box'
+
+export default {
+    name: "Review",
+    components: {
+        name_box
+    }
 }
 </script>
 
 <style>
-
+#review_text {
+    position: absolute;
+    width: 43%;
+    height: 25%;
+    left: 28%;
+    top: 8%;
+}
+.big_cloud {
+    position: absolute;
+    top: 14%;
+}
+#review_big_cloud_left {
+    left: 17%;
+    transform: rotateY(180deg);
+}
+#review_big_cloud_right {
+    left: 71%;
+}
+.name {
+    position: absolute;
+    width: 50%;
+    height: 50%;
+    top: 15%;
+    left: 5%;
+}
+.name-table {
+    display: grid;
+    grid-template-columns: repeat(5, 27%);
+    position: absolute;
+    top: 50%;
+    left: 29%;
+}
 </style>
